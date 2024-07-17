49e298c2688d4b7adb9fad035b28698ac1c6c5a8
Rajat <rajatdoit18@gmail.com>
2024-07-17 20:04:01 +0530

    Added code to save application forms to database

--- job_application/models.py
+++ job_application/models.py
@@ -1,10 +1,11 @@
 from django.db import models
 
+
 # Create your models here.
 class Form(models.Model):
     first_name = models.CharField(max_length=80)
     last_name = models.CharField(max_length=80)
-    email = models.CharField(max_length=80)
+    email = models.EmailField()
     date = models.DateField()
     occupation = models.CharField(max_length=80)
 
--- job_application/views.py
+++ job_application/views.py
@@ -1,5 +1,32 @@
 from django.shortcuts import render
+from .forms import ApplicationForm
+from .models import Form
+from django.contrib import messages
 
-# Create your views here.
+
 def index(request):
-    return render(request, "index.html")
+    if request.method == "POST":
+        form = ApplicationForm(request.POST)
+        print("Form submitted")
+
+        if form.is_valid():
+            print("Form is valid")
+            first_name = form.cleaned_data["first_name"]
+            last_name = form.cleaned_data["last_name"]
+            email = form.cleaned_data["email"]
+            date = form.cleaned_data["date"]
+            occupation = form.cleaned_data["employment"]
+
+            Form.objects.create(first_name=first_name, last_name=last_name,
+                                email=email, date=date, occupation=occupation)
+            messages.success(request, "Application submitted successfully!")
+
+        else:
+            print("Form is not valid")
+            print(form.errors)
+
+    else:
+        form = ApplicationForm()
+        print("GET request")
+
+    return render(request, "index.html", {'form': form})
